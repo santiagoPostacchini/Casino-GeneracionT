8ec05c14a702f9df4c0a7ab8a6350a875e3bf87a
Santiago Postacchini <90282556+santiagoPostacchini@users.noreply.github.com>
2022-11-14 17:11:49 -0300

    Add files via upload

---
+++ Casino-React-ET21/casino/src/components/PutForm.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import {useForm} from "react-hook-form"
+
+const PutUserForm = () => {
+
+    const { handleSubmit, register, formState: { errors }, watch} = useForm();
+
+    async function onSubmit(data){
+        console.log(data)
+        await axios.put('http://localhost:3001/usuarios', data).then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        window.location.reload();
+        
+    }
+
+    return <div>
+        <h2>Modificar Usuario</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label>Nombre </label>
+                <input type="text" {...register('Nombre', {
+                    required: true
+                })}/>
+            </div>
+            <div>
+                <label>Mail </label>
+                <input
+                    type="email" {...register("Mail", {
+                        required: true,
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: " Mail invalido"
+                        }
+                    })}
+                />
+                {errors.email && errors.email.message}
+            </div>
+            <div>
+                <label>Contraseña </label>
+                <input {...register("Pass", {
+                    required: true,
+                    validate: (val, string) => {
+                    if (watch('confirm_password') !== val) {
+                    return "Las contraseñas no coinciden";
+                    }
+                },
+                })}
+                />
+            </div>
+            <div>
+                <label>Confirmar Contraseña </label>
+                <input {...register("confirm_password", {
+                    required: true
+                    })}
+                />
+            </div>
+            <input type="submit" value="Enviar"/>
+        </form>
+    </div>
+}
+
+export default PutUserForm;
